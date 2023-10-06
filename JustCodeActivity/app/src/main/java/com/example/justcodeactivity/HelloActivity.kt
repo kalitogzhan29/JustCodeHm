00c2e63bcfe3de0f8daa310505999dd35f69b983
Togzhan Kali <t_kali@kbtu.kz>
2023-10-07 02:44:16 +0600

    ksjdns kjdnj

---
+++ JustCodeActivity/app/src/main/java/com/example/justcodeactivity/HelloActivity.kt
@@ -0,0 +1,23 @@
+package com.example.justcodeactivity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.justcodeactivity.databinding.ActivityHelloBinding
+
+class HelloActivity:AppCompatActivity() {
+    private lateinit var binding:ActivityHelloBinding
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        binding= ActivityHelloBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        intent.extras?.let{
+            val name=it.getString(ArgumentKey.NAME.name)
+
+            val newName = it.getString(Intent.EXTRA_TEXT)
+            binding.helloView.text=getString(R.string.hello_any, name)
+        }
+    }
+}
